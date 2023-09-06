215ccbcf0185f6ffe4c753ed468968600f918459
Tarik Amar <tarik.amar@gmail.com>
2023-09-06 14:08:33 +0200

    Ingredients - List from stub

--- app/routes/ingredients/index.tsx
+++ app/routes/ingredients/index.tsx
@@ -1,7 +1,34 @@
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+export const loader = async () => {
+  return json({
+    ingredients: [
+      {
+        slug: 'test'
+        , title: 'title?'
+      }
+      , {
+        slug: 'test 2'
+        , title: 'title !'
+      }
+    ]
+  })
+}
+
 export default function Ingredients() {
+  const {ingredients} = useLoaderData<typeof loader>()
+
   return (
     <main>
       <h2>Ingrédients</h2>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient.slug}>
+            {ingredient.title}
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
